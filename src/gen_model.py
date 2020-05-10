631c76a7fa3293922186cb4b65fa785d1bd4c99a
Shinichi Morita <shin3tky@gmail.com>
2020-05-10 16:38:41 +0900

    Added to generate __toString().
    - replace from direct output with print() to build string with list.

--- src/gen_model.py
+++ src/gen_model.py
@@ -3,15 +3,16 @@ import xml.etree.ElementTree as ET
 
 
 def generate_converter_method(table):
+    buff = []
     table_name = table.attrib["name"]
-    print(
+    buff.append(
         "    public function to"
         + to_pascal_case(table_name)
         + "(ORM $item) : "
         + to_pascal_case(table_name)
         + " {"
     )
-    print(
+    buff.append(
         "        $"
         + to_camel_case(table_name)
         + " = new "
@@ -21,7 +22,7 @@ def generate_converter_method(table):
 
     for field in table.findall("./field"):
         field_name = field.attrib["Field"]
-        print(
+        buff.append(
             "        $"
             + to_camel_case(table_name)
             + "->"
@@ -31,29 +32,59 @@ def generate_converter_method(table):
             + ";"
         )
 
-    print("        return $" + to_camel_case(table_name) + ";")
-    print("    }")
+    buff.append("        return $" + to_camel_case(table_name) + ";")
+    buff.append("    }")
+    return buff
+
+
+def generate_tostring_method(table):
+    buff = []
+    buff.append("    public function __toString() {")
+    buff.append("        $result = [];")
+
+    for field in table.findall("./field"):
+        field_name = field.attrib["Field"]
+        buff.append(
+            "        $result[] = '"
+            + to_camel_case(field_name)
+            + ":[' . $this->"
+            + to_camel_case(field_name)
+            + " . ']"
+            + "';"
+        )
+
+    buff.append("        return implode(',', $result);")
+    buff.append("    }")
+    return buff
 
 
 def generate_model_class(filename):
+    buff = []
     tree = ET.parse(filename)
     root = tree.getroot()
     for table in root.findall("./database/table_structure"):
         table_name = table.attrib["name"]
-        print("class " + to_pascal_case(table_name) + " {")
+        buff.append("class " + to_pascal_case(table_name) + " {")
 
         # generate properties.
         for field in table.findall("./field"):
             field_comment = field.attrib["Comment"]
             field_name = field.attrib["Field"]
-            print("    // " + field_comment)
-            print("    public $" + to_camel_case(field_name) + ";")
-            print("")
+            buff.append("    // " + field_comment)
+            buff.append("    public $" + to_camel_case(field_name) + ";")
+            buff.append("")
 
         # generate converter.
-        generate_converter_method(table)
+        method = generate_converter_method(table)
+        buff.append("\n".join(method))
 
-        print("}")
+        buff.append("")
+
+        tostring = generate_tostring_method(table)
+        buff.append("\n".join(tostring))
+
+        buff.append("}")
+    return buff
 
 
 def to_pascal_case(snake_str):
@@ -70,6 +101,7 @@ if __name__ == "__main__":
     args = sys.argv
     if 2 == len(args):
         filename = args[1]
-        generate_model_class(filename)
+        buff = generate_model_class(filename)
+        print("\n".join(buff))
     else:
         print("Usage: python gen_model.py [filename.xml]")
